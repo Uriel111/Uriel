d0aeaab3c022016010952bd07a01cf5b9f16d21e
wangjian <421695865@qq.com>
2021-03-31 11:16:58 -0700

    today lc + cmake

---
+++ LeetCodeSrc/StupidFactorial.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+    int clumsy(int num)
+    {
+        int res = 0;
+        if (num / 4)
+        {
+            res += num * (num - 1) / (num - 2) + num - 3;
+            num -= 4;
+        }
+        else
+        {
+            switch (num % 4)
+            {
+            case 0:
+                return res + 0;
+            case 1:
+                return res + 1;
+            case 2:
+                return res + 2 * 1;
+            case 3:
+                return res + 3 * 2 / 1;
+            }
+        }
+        while (num / 4)
+        {
+            res = res - num * (num - 1) / (num - 2) + num - 3;
+            num -= 4;
+        }
+        switch (num % 4)
+        {
+        case 0:
+            return res - 0;
+        case 1:
+            return res - 1;
+        case 2:
+            return res - 2 * 1;
+        case 3:
+            return res - 3 * 2 / 1;
+        }
+        return res;
+    }
+};
