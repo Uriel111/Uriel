1a2e619090f258e6f17cd616cffee2166bf5682b
wangjian <421695865@qq.com>
2021-04-24 10:15:25 -0700

    today lc

--- LeetCodeSrc/isMatch.cpp
+++ LeetCodeSrc/isMatch.cpp
@@ -1,22 +1,55 @@
 #include <string>
-#include<limits>
+#include <limits>
+#include <vector>
+
+using bVector = std::vector<bool>;
+using b2DVector = std::vector<bVector>;
 using string = std::string;
 class Solution
 {
+public:
     bool isMatch(string s, string p)
     {
-        int index = 0;
-        while (index < s.size())
+        int sSize = s.size();
+        int pSize = p.size();
+        b2DVector bMatrix;
+        auto match = [&](int i, int j) {
+            if (i == 0)
+                return false;
+            if (p[j - 1] == '.')
+                return true;
+            return s[i - 1] == p[j - 1];
+        };
+        bMatrix.resize(sSize + 1);
+        for (int i = 0; i != bMatrix.size(); ++i)
+            bMatrix[i].resize(pSize + 1);
+        bMatrix[0][0] = true;
+        for (int i = 0; i != bMatrix.size(); ++i)
         {
-            for (int pIndex = 0; pIndex != p.size(); ++pIndex)
+            for (int j = 1; j != bMatrix[i].size(); ++j)
             {
-                if(s[index] == p[pIndex] || s[index] == '.')
+                if (p[j - 1] != '*')
                 {
-                    ++index;
-                    continue;
-                }hu
-
+                    if (match(i, j))
+                    {
+                        bMatrix[i][j] = bMatrix[i - 1][j - 1];
+                    }
+                    else
+                        bMatrix[i][j] = false;
+                }
+                else
+                {
+                    if (match(i, j - 1))
+                    {
+                        bMatrix[i][j] = bMatrix[i - 1][j] || bMatrix[i][j - 2];
+                    }
+                    else
+                    {
+                        bMatrix[i][j] = bMatrix[i][j - 2];
+                    }
+                }
             }
         }
+        return bMatrix[s.size()][p.size()];
     }
 };
